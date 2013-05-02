e7ac93ef729ad623a8d159bd089b3c6613f8ba28
Ollie Harlowe <oharlowe@vt.edu>
2013-05-02 18:55:38 -0400

    Passwd and shadow files created

--- src/examples/login.c
+++ src/examples/login.c
@@ -26,67 +26,18 @@ main (void)
   int passwd_fd = open ("etc/passwd");
 	int shadow_fd = open ("etc/shadow");
 
-	/* Create passwd if it does not exist and open it adding
-   * root to passwd. */
+	/* Exit if etc/passwd is not present. */
   if (passwd_fd == -1)
 	{
-		create ("etc/passwd", 10000);
-		passwd_fd = open ("etc/passwd");
-		write (passwd_fd, "root:x:0:0\n", 11);
-		write (passwd_fd, "kevin:x:1:1\n", 12);
-		write (passwd_fd, "ollie:x:2:2\n", 12);
-		write (passwd_fd, "mike:x:3:3\n", 11);
+		printf("File etc/passwd not found.\n");
+		return -1;
 	}
 
-  /* Create shadow if it does not exist and open it adding
-   * root to shadow. */
+  /* Exit if etc/shadow is not present. */
   if (shadow_fd == -1)
 	{
-		create ("etc/shadow", 10000);
-		shadow_fd = open ("etc/shadow");
-		
-		char hashed_passwd [33];
-		char root [5] = "root";
-		char kevin [6] = "kevin";
-		char ollie [6] = "ollie";
-		char mike [5] = "mike";
-		char colon [1] = ":";
-		char new_line [1] = "\n";
-
-		char user_entry [100];
-
-		md5 ((uint8_t *)root, strlen(root), hashed_passwd);
-		strlcpy (user_entry, root, strlen (root) + 1);
-		strlcat (user_entry, colon, strlen (user_entry) + strlen (colon) + 1);
-		strlcat (user_entry, hashed_passwd, strlen (user_entry) + strlen (hashed_passwd) + 1);
-		strlcat (user_entry, new_line, strlen (user_entry) + strlen (new_line) + 1);
-		write (shadow_fd, user_entry, strlen (user_entry) + 1);
-
-		memset (user_entry, 0, 100);
-		md5 ((uint8_t *)kevin, strlen(kevin), hashed_passwd);
-		strlcpy (user_entry, kevin, strlen (kevin) + 1);
-		strlcat (user_entry, colon, strlen (user_entry) + strlen (colon) + 1);
-		strlcat (user_entry, hashed_passwd, strlen (user_entry) + strlen (hashed_passwd) + 1);
-		strlcat (user_entry, new_line, strlen (user_entry) + strlen (new_line) + 1);
-		write (shadow_fd, user_entry, strlen (user_entry) + 1);
-	
-		memset (user_entry, 0, 100);
-		md5 ((uint8_t *)ollie, strlen(ollie), hashed_passwd);
-		strlcpy (user_entry, ollie, strlen (ollie) + 1);
-		strlcat (user_entry, colon, strlen (user_entry) + strlen (colon) + 1);
-		strlcat (user_entry, hashed_passwd, strlen (user_entry) + strlen (hashed_passwd) + 1);
-		strlcat (user_entry, new_line, strlen (user_entry) + strlen (new_line) + 1);
-		write (shadow_fd, user_entry, strlen (user_entry) + 1);
-
-		memset (user_entry, 0, 100);
-		md5 ((uint8_t *)mike, strlen(mike), hashed_passwd);	
-		strlcpy (user_entry, mike, strlen (mike) + 1);
-		strlcat (user_entry, colon, strlen (user_entry) + strlen (colon) + 1);
-		strlcat (user_entry, hashed_passwd, strlen (user_entry) + strlen (hashed_passwd) + 1);
-		strlcat (user_entry, new_line, strlen (user_entry) + strlen (new_line) + 1);
-		write (shadow_fd, user_entry, strlen (user_entry) + 1);
-
-	
+		printf("File etc/shadow not found.\n");
+		return -1;
 	}
 
   /* Attempt first verification. */
